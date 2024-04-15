1a85307e858619151312debf10c89935a8cbf1f2
tuna <tuna@woowahan.com>
2024-04-15 23:58:57 +0900

    feat: 전역 에러 핸들러 추가

---
+++ src/main/kotlin/org/seoul/morlatjanghak/common/GlobalExceptionHandler.kt
@@ -0,0 +1,15 @@
+package org.seoul.morlatjanghak.common
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handle(e: IllegalArgumentException): ResponseEntity<String> = ResponseEntity.badRequest().body(e.message)
+
+    @ExceptionHandler(Exception::class)
+    fun handle(e: Exception): ResponseEntity<Exception> = ResponseEntity.internalServerError().body(e)
+}
