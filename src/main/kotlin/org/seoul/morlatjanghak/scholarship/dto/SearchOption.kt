e9329c8ef644377628b44b88274099eff8f08304
tuna <tuna@woowahan.com>
2024-04-25 00:03:13 +0900

    feat: 장학금 검색 조건 수정

--- src/main/kotlin/org/seoul/morlatjanghak/scholarship/dto/SearchOption.kt
+++ src/main/kotlin/org/seoul/morlatjanghak/scholarship/dto/SearchOption.kt
@@ -5,11 +5,15 @@ import org.springframework.data.domain.Sort
 data class SearchOption(
     val keyword: String?,
     val viewCount: Boolean = false,
+    val deadline: Boolean = false,
 ) {
     fun getSort(): Sort {
         val sort = Sort.unsorted()
-        if (viewCount) {
+        viewCount.let {
             sort.and(Sort.by(Sort.Order.desc("viewCount")))
+        }
+        deadline.let {
+            sort.and(Sort.by(Sort.Order.asc("endDate")))
         }
 
         return sort
