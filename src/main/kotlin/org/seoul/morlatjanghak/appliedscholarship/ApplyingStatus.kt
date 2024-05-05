8b63f3adb7ecdc18e27633cef1b89a1ffd29621c
tuna <tuna@woowahan.com>
2024-05-06 03:18:03 +0900

    feat: 지원 상태인 경우는 무조건 저장된 상태로 취급

--- src/main/kotlin/org/seoul/morlatjanghak/appliedscholarship/ApplyingStatus.kt
+++ src/main/kotlin/org/seoul/morlatjanghak/appliedscholarship/ApplyingStatus.kt
@@ -2,6 +2,7 @@ package org.seoul.morlatjanghak.appliedscholarship
 
 enum class ApplyingStatus {
     NOTHING, // 아무 상태도 아님
+    SAVED, // 저장 상태
     PLANNED, // 지원 예정
     APPLIED, // 지원 완료
     PASSED, // 합격
