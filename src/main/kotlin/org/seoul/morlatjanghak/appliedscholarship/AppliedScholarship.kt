e050cc0aac3c46918ce32b79cfddecca8f2c176d
tuna <tuna@woowahan.com>
2024-05-05 16:32:21 +0900

    fix: enum 저장타입 varchar로 변경

--- src/main/kotlin/org/seoul/morlatjanghak/appliedscholarship/AppliedScholarship.kt
+++ src/main/kotlin/org/seoul/morlatjanghak/appliedscholarship/AppliedScholarship.kt
@@ -1,5 +1,6 @@
 package org.seoul.morlatjanghak.appliedscholarship
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
@@ -10,13 +11,14 @@ import org.seoul.morlatjanghak.common.BaseTimeEntity
 
 @Entity
 class AppliedScholarship(
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     var memberId: String,
     var scholarshipId: Long,
     @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(50) COMMENT '지원 상태'")
     var status: ApplyingStatus = ApplyingStatus.NOTHING,
 ) : BaseTimeEntity() {
 
